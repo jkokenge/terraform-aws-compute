5fdc9efb949ea7a4f01d7873ee2d4a9dc9fc641e
Ubuntu <ubuntu@ip-172-31-65-96.ec2.internal>
2021-08-19 14:46:21 +0000

    added in tf code

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "aws_regions" {}
+variable "instance_count" {
+    default = 1
+}
+variable "instance_type"{
+    default = "t3.micro"
+}
+
+variable "public_sg" {
+
+}
+
+variable "public_subnets" {}
+variable "vol_size" {
+    default = 8    
+}
+variable "public_key_material" {}
+variable "key_name" {
+    default = "keymtc"
+}
